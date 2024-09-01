527e0e872fb3929ee9cb1160a899364ddf4fbdbb
zainali28 <zainali123113@gmail.com>
2024-09-01 23:32:32 +0500

    removed restart button

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ var capture = Array(9).fill(false);
 function keyStrokeDown(event) {
     const key = event.key;
     var boxNum = (key >= 1 && key <= 9) ? (key - 1) : null;
-    // document.getElementsByName("Box").forEach((element, index) => { ((index === boxNum) & capture[boxNum]) && (element.disabled = true); });
     const elem = document.getElementsByClassName('red-box');
     (boxNum !== null) && (elem[boxNum].setAttribute(
         "style", `width: ${enlarged[boxNum] ? "500px" : "100px"}; height: ${enlarged[boxNum] ? "500px" : "100px"}; background-color: ${enlarged[boxNum] ? "orangered" : "orange"}; box-shadow: ${enlarged[boxNum] ? "orange" : "orangered"} ${enlarged[boxNum] ? "20px" : "10px"} ${enlarged[boxNum] ? "20px" : "10px"}; content-visibility: visible;`
     ));
-
-    (key === "r" ) && restart(); 
 }
 
 function keyStrokeUp(event) {
     const key = event.key;
     var boxNum = (key >= 1 && key <= 9) ? (key - 1) : null;
-    // document.getElementsByName("Box").forEach((element, index) => { ((index === boxNum) & capture[boxNum]) && (element.disabled = true); });
     const elem = document.getElementsByClassName('red-box');
     capture[boxNum] = !clicked[boxNum] && true;
     enlarged[boxNum] = !enlarged[boxNum];
@@ -30,18 +26,6 @@ function keyStrokeUp(event) {
 document.addEventListener('keydown', keyStrokeDown);
 document.addEventListener('keyup', keyStrokeUp);
 
-function restart() {
-    const elem = document.getElementsByClassName('red-box');
-    enlarged = Array(9).fill(false);
-    clicked = Array(9).fill(true);
-    capture = Array(9).fill(false);
-
-    for(let i = 0; i < 9; i++) {
-        elem[i].setAttribute(
-            "style", `width: 100px; height: 100px; background-color: orange; opacity: 1;`
-        );
-    }
-}
 
 function onClick(boxNum) {
     const elem = document.getElementsByClassName('red-box');
